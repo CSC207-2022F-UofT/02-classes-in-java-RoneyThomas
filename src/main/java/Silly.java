d49d497c95ad180c0ea9055940003adff497fd91
RoneyThomas <roneythomas6@gmail.com>
2022-10-04 03:14:14 -0400

    Completed all TODO

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * This file contains a few exercises to familiarize you with specific
@@ -79,7 +80,16 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two strings as an argument.
+     *
+     * @param firstString for this Silly instance's name.
+     * @param secondString for this Silly instance's name.
+     */
+    public Silly(String firstString, String secondString) {
+        this.name = firstString + secondString;
+    }
 
 
 
@@ -116,7 +126,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +144,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +172,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return Objects.equals(this.name, other.name);
     }
 
     /**
@@ -194,6 +206,13 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if (this.name.length() > other.name.length()) {
+            return 1;
+        } else if(this.name.length() == other.name.length()){
+            return 0;
+        } else {
+            return -1;
+        }
     }
 
     /*
